f6313533be15f27013b50942313847c9d1f5652f
MojeshReddy <mojeshreddylingala@gmail.com>
2025-07-11 10:29:09 +0530

    Brief, descriptive message about your changes

--- program.py
+++ program.py
@@ -1,1 +1,10 @@
+import time
 
+def main():
+    print("Hello World from ECS Fargate!")
+    print("Sleeping for 20 seconds...")
+    time.sleep(20)
+    print("Waking up! Goodbye!")
+
+if __name__ == "__main__":
+    main()
